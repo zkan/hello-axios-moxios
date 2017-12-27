c81e5ad66f820bd09c77b03100f06dfb479cebaa
zkan <kan@prontomarketing.com>
2017-12-27 09:13:04 +0700

    Try different version

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import axios from 'axios'
 
 const printTitle = (text) => console.log(text)
 
-const getPost = async (postId) => {
+//const getPost = async (postId) => {
+  //const apiUrl = 'https://jsonplaceholder.typicode.com/posts/' + postId
+  //const res = await axios.get(apiUrl)
+  //printTitle(res.data.title)
+//}
+
+const getPost = (postId) => {
   const apiUrl = 'https://jsonplaceholder.typicode.com/posts/' + postId
-  const res = await axios.get(apiUrl)
-  printTitle(res.data.title)
+  axios.get(apiUrl)
+    .then((res) => printTitle(res.data.title))
 }
 
 getPost(1)
